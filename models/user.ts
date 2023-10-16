7d628d73a893e0365199e8052120a128018a37c6
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-15 21:26:03 -0500

    feat: add new user model

---
+++ models/user.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+interface IUser {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  status: "member" | "admin";
+}
+// Create schema
+const userSchema = new mongoose.Schema<IUser>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+});
+
+// Compile into model and export
+const User = mongoose.model("User", userSchema);
+
+export default User;
