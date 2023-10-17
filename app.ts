ee07de6969c941bfa213275de68319edbab7cc08
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-17 14:34:54 -0500

    chore: Define use of LocalStrategy

--- app.ts
+++ app.ts
@@ -25,6 +25,29 @@ async function connecToMongoDB() {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// Declare Strategy to use on passport
+passport.use(
+  new Strategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, {
+          message: "Incorrect username or password.",
+        });
+      }
+      const passwordsAreEqual = await bcrypt.compare(password, user.password);
+      if (!passwordsAreEqual) {
+        return done(null, false, {
+          message: "Incorrect username or password.",
+        });
+      }
+      return done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
 // Middleware to read request body
 app.use(express.json());
 app.use(passport.initialize());
