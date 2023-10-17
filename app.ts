816c2050eb86d88d22e0aea1c234df5e5d3f0f33
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-17 14:32:45 -0500

    chore: Apply middlewares to read requests

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
+import userRouter from "./resources/user/user.router";
+import { Strategy } from "passport-local";
 import mongoose from "mongoose";
 import passport from "passport";
-import { Strategy } from "passport-local";
 import path from "path";
+import session from "express-session";
 import express from "express";
 import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
+import User from "./resources/user/user.model";
 // Load env variables
 dotenv.config();
 
@@ -12,6 +16,7 @@ const app = express();
 // Connect to mongoDB
 connecToMongoDB().catch((err) => console.log(err));
 async function connecToMongoDB() {
+  console.log("Starting connection to MongoDB");
   await mongoose.connect(process.env.MONGODB_URI);
   console.log("Connected to MongoDB!");
 }
@@ -20,12 +25,17 @@ async function connecToMongoDB() {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// Middleware to read request body
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
 // Set public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res, next) => {
-  res.send("Hola mundo");
-});
+// Use routers
+app.use("/", userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Listening to PORT: " + process.env.PORT);
