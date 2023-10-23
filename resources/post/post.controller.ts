7bd4d31d10b3a71a61bb5d6aa070b93c9f76e423
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-22 21:39:43 -0500

    feat:  Add a new Post or delete a post

--- resources/post/post.controller.ts
+++ resources/post/post.controller.ts
@@ -1,7 +1,52 @@
 import asyncHand from "express-async-handler";
 import Post from "./post.model";
+import { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 export const showPosts = asyncHand(async (req, res, next) => {
-  const allPosts = await Post.find({});
-  res.render("index", {posts: allPosts});
+  const allPosts = await Post.find({}).populate("author", "username").exec();
+  res.render("index", { posts: allPosts });
+});
+
+export const getPostForm = (req: Request, res: Response) => {
+  res.render("new-post");
+};
+
+export const addNewPost = [
+  body("title").isLength({ min: 1 }),
+  body("content").isLength({ min: 1 }),
+  asyncHand(async (req, res) => {
+    const result = validationResult(req);
+
+    if (!result.isEmpty()) {
+      res.render("new-post", {
+        errors: result.array(),
+      });
+      return;
+    }
+
+    // NO VALIDATION ERRORS, SAVE POST
+    const newPost = new Post({
+      content: req.body.content,
+      title: req.body.title,
+      author: res.locals.currentUser._id,
+    });
+
+    await newPost.save();
+    res.redirect("/");
+  }),
+];
+
+export const deletePost = asyncHand(async (req, res, next) => {
+  console.log(req.params);
+  res.render("confirm-delete", { post: req.params.id });
+});
+
+export const confirmDeletePost = asyncHand(async (req, res, next) => {
+  if (req.body.value) {
+    const postToDelete = await Post.findByIdAndRemove(req.body.value);
+    console.log("Post to delete", postToDelete);
+  }
+
+  res.redirect("/");
 });
--- resources/post/post.router.ts
+++ resources/post/post.router.ts
@@ -1,9 +1,23 @@
 import express from "express";
-import { showPosts } from "./post.controller";
+import {
+  getPostForm,
+  showPosts,
+  addNewPost,
+  deletePost,
+  confirmDeletePost,
+} from "./post.controller";
 
 const router = express.Router();
 
 /* Index page */
 router.get("/", showPosts);
+
+/* New posts */
+router.get("/new-post", getPostForm);
+router.post("/new-post", addNewPost);
+
+/* Handle delete of message */
+router.get("/delete/:id", deletePost);
+router.post("/delete/:id", confirmDeletePost);
 
 export default router;
